90dcc17316f5ca4dc99fef9d7ec6a695078ac78b
Gayathri Lakshmi <GayathriLakshmi@wipro.com>
2025-04-30 15:26:04 +0530

    A

---
+++ April-30/Assignments/Assignment_5_Gayathri.cs
@@ -0,0 +1,19 @@
+//Reverse of numbers
+
+using System;
+
+public class square
+{
+    static void PrintArray(int[] arr)
+    {
+       for (int i = arr.Length - 1; i >= 0; i--)
+        {
+            Console.WriteLine($"Element {arr.Length - i}: {arr[i]}");
+        }
+    }
+    static void Main()
+    {
+        int[] numbers = {10,20,30,40,50};
+        PrintArray(numbers);
+    }
+}
