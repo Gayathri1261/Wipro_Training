7ce64e6d75a7509f03f162a519e0279690eb1179
Gayathri Lakshmi <GayathriLakshmi@wipro.com>
2025-04-30 15:34:15 +0530

    c

---
+++ April-30/Class codes/funmethod-2.cs
@@ -0,0 +1,16 @@
+// a function need not return anything
+//->it is not mandatory to return the function
+
+using System;
+
+public class square
+{
+   static void GreetUser(string name)
+   {
+       Console.WriteLine("Hello, "+ name);
+   }
+   static void Main()
+   {
+       GreetUser("Amina");
+   }
+}
