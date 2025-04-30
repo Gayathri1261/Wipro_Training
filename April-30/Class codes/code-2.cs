25f868998b1b91555924bcbbd6e311b0302b0e0d
Gayathri Lakshmi <GayathriLakshmi@wipro.com>
2025-04-30 10:52:04 +0530

    c

---
+++ April-30/Class codes/code-2.cs
@@ -0,0 +1,26 @@
+//Switch case:
+
+using System;
+
+public class day
+{
+    public static void Main(string[] args)
+    {
+        int day=3;
+        switch(day)
+        {
+        case 1:
+        Console.WriteLine ("Monday");
+        break;
+        case 2:
+        Console.WriteLine ("Tuesday");
+        break;
+        case 3:
+        Console.WriteLine ("Wednesday");
+        break;
+        default:
+        Console.WriteLine ("Invalid day");
+        break;
+        }
+    }
+}
