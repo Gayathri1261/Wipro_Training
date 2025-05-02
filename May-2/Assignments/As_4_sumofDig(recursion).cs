202f8fd8c69212b2f7379b065cfaaa07957ecf31
Gayathri Lakshmi <GayathriLakshmi@wipro.com>
2025-05-02 15:58:12 +0530

    A

--- May-2/Assignments/As_4_sumofDig(recursion).cs
+++ May-2/Assignments/As_4_sumofDig(recursion).cs
@@ -4,15 +4,15 @@ class Program
 {
     static int SumArray(int[] arr, int index)
     {
-        if (index == arr.Length) 
+        if (index == arr.Length) // when index reaches to array length
         {
-            return 0;
+            return 0;    //It stops recursion returning 0
         }
         else
         {
             
-            Console.Write(arr[index] + " ");
-            return arr[index] + SumArray(arr, index + 1); 
+            Console.Write(arr[index] + " "); //prints the current element
+            return arr[index] + SumArray(arr, index + 1); //recursively calls SumArray with the next index. Adds the current element to the result of the recursive call, which gives the running sum.
         }
     }
 
@@ -23,9 +23,10 @@ class Program
 
         
         Console.Write("Elements in the array: ");
-        int result = SumArray(arr, 0);
+        int result = SumArray(arr, 0); //calls the SumArray starting from index 0.
+                                       // It prints each element and returns the total sum, which is stored in result.
 
         
-        Console.WriteLine($"\nSum of elements in the array is: {result}");
+        Console.WriteLine($"\nSum of elements in the array is: {result}"); After all elements printed,it prints the sum.
     }
 }
