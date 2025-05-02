5b6e75b059194a37c5bc11cc83285b7cf2cff2e6
Gayathri Lakshmi <GayathriLakshmi@wipro.com>
2025-05-02 15:42:02 +0530

    A

--- May-2/Assignments/As_6_printArrayElements.cs
+++ May-2/Assignments/As_6_printArrayElements.cs
@@ -4,15 +4,15 @@ class Program
 {
     static void PrintArray(int[] arr, int index)
     {
-        if (index == arr.Length) 
+        if (index == arr.Length) // if index equal to arr.length,means it reached past the last element
         {
-            return;
+            return;              //so we stop recursion using return statement
         }
         else
         {
-            Console.Write(arr[index] + " ");
+            Console.Write(arr[index] + " "); //first print the current element
             
-            PrintArray(arr, index + 1);
+            PrintArray(arr, index + 1); //call PrintArray() with the next index (index + 1) ,next element is processed in the next recursive call.
         }
     }
     static void Main()
@@ -21,17 +21,17 @@ class Program
         Console.Write("Enter the number of elements in the array: ");
         int n = int.Parse(Console.ReadLine());
         
-        int[] arr = new int[n];
+        int[] arr = new int[n]; //Creates an integer array of size n.
 
         
         Console.WriteLine("Enter the elements of the array:");
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i < n; i++) //Iterates from 0 to n-1, taking n individual inputs from the user.
         {
-            arr[i] = int.Parse(Console.ReadLine());
+            arr[i] = int.Parse(Console.ReadLine()); //Each input is stored in arr.
         }
 
         Console.Write("Array elements: ");
-        PrintArray(arr, 0); 
-        Console.WriteLine(); 
+        PrintArray(arr, 0); // Calls PrintArray starting at index 0.
+        Console.WriteLine(); //it add the new line for after printing all elements.
     }
 }
