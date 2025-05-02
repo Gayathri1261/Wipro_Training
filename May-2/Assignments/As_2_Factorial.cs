87d132ab38e71ec59c5042bce0282a587749bdb4
Gayathri Lakshmi <GayathriLakshmi@wipro.com>
2025-05-02 15:28:05 +0530

    A

--- May-2/Assignments/As_2_Factorial.cs
+++ May-2/Assignments/As_2_Factorial.cs
@@ -5,13 +5,13 @@ class Program
     
     static int Factorial(int n)
     {
-        if (n == 0 || n == 1) 
+        if (n == 0 || n == 1) //if n=0/1 it stops the recursion
         {
             return 1;
         }
         else
         {
-            return n * Factorial(n - 1); 
+            return n * Factorial(n - 1); //if n is more than 1 it will be multiplied
         }
     }
 
@@ -22,17 +22,17 @@ class Program
         int n = int.Parse(Console.ReadLine());
 
         
-        int[] arr = new int[n];
+        int[] arr = new int[n];  //creates an int arr of size n 
 
         
         Console.WriteLine("Enter the elements of the array:");
         for (int i = 0; i < n; i++)
         {
-            arr[i] = int.Parse(Console.ReadLine());
+            arr[i] = int.Parse(Console.ReadLine()); //after entering the elements stores each number in arr[i]
         }
 
         
-        int lastElement = arr[arr.Length - 1];
+        int lastElement = arr[arr.Length - 1]; //last element of the array
 
        
         int result = Factorial(lastElement);
