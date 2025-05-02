cf4f9215870fe97eee2829b421dfe5aa5ab23a26
Gayathri Lakshmi <GayathriLakshmi@wipro.com>
2025-05-02 15:49:00 +0530

    A

--- May-2/Assignments/As_3_Fibonacci.cs
+++ May-2/Assignments/As_3_Fibonacci.cs
@@ -5,13 +5,13 @@ class Program
     // Recursive function to calculate Fibonacci number
     static int Fibonacci(int n)
     {
-        if (n <= 1)
+        if (n <= 1)  //if n is 0,return 0    if n is 1, return 1  it prevents from infinite recursion
         {
             return n;
         }
         else
         {
-            return Fibonacci(n - 1) + Fibonacci(n - 2);
+            return Fibonacci(n - 1) + Fibonacci(n - 2);  //it returns the sum of previous 2 fibonacci numbers.
         }
     }
 
@@ -25,7 +25,9 @@ class Program
         Console.WriteLine($"Fibonacci numbers up to position {n}:");
         for (int i = 0; i <= n; i++)
         {
-            Console.WriteLine(Fibonacci(i));
+            Console.WriteLine(Fibonacci(i)); //For each index i, the program calls the recursive Fibonacci(i) function and prints the result.
+
+
         }
     }
 }
