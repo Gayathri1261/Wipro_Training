d925acc204c6b4fb55726462d2596d1e697dcbd6
Gayathri Lakshmi <GayathriLakshmi@wipro.com>
2025-05-02 11:20:50 +0530

    A

--- May-2/Assignments/As_1_PalindromeOrNot.cs
+++ May-2/Assignments/As_1_PalindromeOrNot.cs
@@ -5,9 +5,12 @@ class Program
     // Method using a for loop to check palindrome
     static bool IsPalindrome(string word)
     {
-        for (int i = 0; i < word.Length / 2; i++)
+        for (int i = 0; i < word.Length / 2; i++) //  word.Length / 2,we only check halfway
         {
-            if (word[i] != word[word.Length - 1 - i])
+            if (word[i] != word[word.Length - 1 - i]) //i = 0
+                                                      //  word[0] = 'm'
+                                                      //  word[5 - 1 - 0] = word[4] = 'm'
+                                                       //  'm' == 'm' → ok
                 return false;
         }
         return true;
@@ -17,7 +20,7 @@ class Program
     {
         string[] inputs = { "racecar", "madam" };
 
-        foreach (string word in inputs)
+        foreach (string word in inputs) //This loop goes through each word in the array.
         {
             if (IsPalindrome(word))
                 Console.WriteLine($"'{word}' is a palindrome.");
