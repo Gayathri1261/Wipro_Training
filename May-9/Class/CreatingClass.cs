75f8dd6cddcd16ef3beb6fa9f26e75557690ce99
Gayathri <gayathri@wipro.com>
2025-05-09 10:17:00 +0530

    C

---
+++ May-9/Class/CreatingClass.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Security.Cryptography;
+using ConsoleApp1;
+
+    class Student
+    {
+        //Fields/properties
+        public int RollNumber;
+        public string Name;
+        public int Marks;
+
+        //Method to print student details
+        public void PrintDetails()
+        {
+            Console.WriteLine($"Roll number: {RollNumber}");
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Marks: {Marks}");
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            //Creating an object of student
+            Student student1 = new Student();
+            //Assigning values
+            student1.RollNumber = 101;
+            student1.Name = "Amit";
+            student1.Marks = 85;
+            //Printing details
+            student1.PrintDetails();
+        }
+    }
+
