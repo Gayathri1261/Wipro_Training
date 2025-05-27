0edce42fabc675cb9f30cf2ae0a8bf2c63aa6476
Gayathri <gayathri@wipro.com>
2025-05-27 15:08:58 +0530

    A

---
+++ May-27/Assignment/WebApplication10/WebApplication10/Controllers/About Controller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication10.Controllers
+{
+    public class About_Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
