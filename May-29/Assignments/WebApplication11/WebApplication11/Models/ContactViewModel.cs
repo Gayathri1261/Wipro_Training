ccb867fedbd9ed30bc3fb4882f148e208762bf0d
Gayathri <gayathri@wipro.com>
2025-05-29 12:10:22 +0530

    A

---
+++ May-29/Assignments/WebApplication11/WebApplication11/Models/ContactViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication11.Models
+{
+    public class ContactViewModel
+    {
+        [Required]
+        [Display(Name = "Full Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string EmailAddress { get; set; }
+
+        [Required]
+        [StringLength(1000, ErrorMessage = "Message too long.")]
+        public string Message { get; set; }
+
+    }
+}
---
+++ May-29/Assignments/WebApplication11/WebApplication11/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication11.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string JobTitle { get; set; }
+        public string Department { get; set; }
+        public decimal Salary { get; set; }
+       
+        
+    }
+}
